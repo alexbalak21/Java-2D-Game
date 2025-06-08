ff1800e35a45c8191e9e2d27fa9a216ea389a2fc
Alex Balak <alex.balak@outlook.com>
2025-06-08 22:08:14 +0200

    Testing with classes

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,11 @@ package main;
 
 import javax.swing.JFrame;
 
+
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        int age = MyClass.AGE;
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
---
+++ src/main/MyClass.java
@@ -0,0 +1,5 @@
+package main;
+
+public class MyClass {
+    public static final int AGE = 18;
+}
